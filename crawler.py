3476b5a4efffe9019d5d615b8be218637ab69358
Jose Guzman <joseguzmanzamora@gmail.com>
2018-11-21 11:28:43 -0600

    cambio de la vida

--- crawler.py
+++ crawler.py
@@ -1,22 +1,82 @@
-from pygments import highlight
+from pygments.lexers import JavascriptLexer
 from pygments import lex
-from pygments.lexers.javascript import JavascriptLexer
-from pygments.formatters.html import HtmlFormatter
+from pygments.token import Token
+from pygments.token import String, string_to_tokentype
 
 lexer = JavascriptLexer()
 
-archivo = open('output.html', 'r+')
-code = open('index.js', 'r+').read()
-formatter = HtmlFormatter(lineous=True, nowrap=False, cssclass="source")
+#File
+filepath = 't.js'
 
-tokens = lex(code, lexer)
-results = []
+#File open
+fp = open(filepath).read()
+
+#Code insertion
+tokens = lex(fp, lexer)
+code = []
 for x in tokens:
-    results.append(x)
+    code.append(x)
 
-contador = 0
-for y in results:
-    if y[1] == "require":
-        print(results[contador + 2][1])
-    contador += 1
 
+def menu():
+        print('\n--------- Crawler JS ---------\n')
+        print('Press (1,2,3,4,5,6,7,8) to choose\n')
+        print('1. Functions\n2. Variables \n3. Reserved Keywords\n4. Builtin \n5. Operators \n6. Constants \n7. Literals \n8. Full Lexer File ')
+        x = input("> ")
+
+        if x == '8':
+                getVars()
+        elif x == '9':
+                functions()
+        else: 
+                print("Error, please enter a valid number. \n")
+                menu()
+
+
+
+#Imprimir el atibuto según el Token
+
+def getVars():
+        cont = 0
+        cont2=0
+        print('\n ------------------- variables definidas por var --------------------\n')
+        for i in code:
+                if i[1] == 'var':
+                        print(code[cont+2][1])
+                cont += 1
+        print("\n")
+        print('\n ------------------- variables definidas por Let --------------------\n')
+        for j in code: 
+                if j[1] == 'let':
+                        print(code[cont2+2][1])
+                cont2 += 1
+        print("\n")
+
+
+            
+def functions():
+        print('\n ------------------- Funciones --------------------\n')
+        contador = 0
+        for y in code:
+                if y[1] == 'function':
+                        if len(code[contador + 2][1]) > 1:
+                                print("Name", code[contador + 2][1])
+                                valor = code[contador + 4][1]
+                                contador_aux = 4
+                                while valor != ")":
+                                        print("Parametro", valor)
+                                        contador_aux += 1
+                                        valor = code[contador + contador_aux][1]
+                        else:
+                                print("No tiene nombre.")
+                                valor = code[contador + 3][1]
+                                contador_aux = 3
+                                while valor != ")":
+                                        print("Parametro", valor)
+                                        contador_aux += 1
+                                        valor = code[contador + contador_aux][1]
+                contador += 1
+
+
+
+menu()
